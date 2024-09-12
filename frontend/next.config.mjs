ca8b542a121b6cfb65557304f1b4c4d57b5134bb
khushal <khushal2004a@gmail.com>
2024-09-13 00:58:06 +0530

    added dscvr metadata

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["lh3.googleusercontent.com", "seeklogo.com", "upload.wikimedia.org", "img.cryptorank.io"]
+        domains: ["lh3.googleusercontent.com", "seeklogo.com", "upload.wikimedia.org", "img.cryptorank.io", "my-canvas.com"]
     }
 };
 
